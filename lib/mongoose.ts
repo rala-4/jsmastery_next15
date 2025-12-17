09d197ed915e20b52be4befed4cae12bba649985
Rala Kous <rolenokous@gmail.com>
2025-12-17 10:00:08 +0300

    create user model

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,5 +1,7 @@
-import mongoos, { Mongoose } from "mongoose";
-const MONGODB_URI = process.env.MONGODB_URI as string | null;
+import mongoose, { Mongoose } from "mongoose";
+const MONGODB_URI = process.env.MONGODB_URI as string;
+
+//*If you just write mongoose.connect() directly in your file, every time you save a file or an API route is hit, your app will create a new connection. Eventually, you will crash the database with "Too Many Connections."
 if (!MONGODB_URI) {
   throw new Error("MONGODB_URI is not define");
 }
@@ -7,6 +9,8 @@ interface MongooseCache {
   conn: Mongoose | null;
   promise: Promise<Mongoose> | null;
 }
+//*2. The "Global Shelf" (The Hack)
+//*We attach our connection variable to the global object. The global object is specific to Node.js and sits "outside" your file. It survives the hot reloads.
 declare global {
   var mongoose: MongooseCache;
 }
@@ -19,7 +23,7 @@ const dbConnect = async (): Promise<Mongoose> => {
     return cached.conn;
   }
   if (!cached.promise) {
-    cached.promise = mongoos
+    cached.promise = mongoose
       .connect(MONGODB_URI, { dbName: "devflow" })
       .then((result) => {
         console.log("done");
